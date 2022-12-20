9ed26b8cb1978b3e63f698efda318a039e009a4a
Mbazu Daniel <Mbazudaniel97@gmail.com>
2022-12-20 10:16:38 +0100

    update pascal

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -8,7 +8,7 @@ def pascal_triangle(n):
             triangle.append([1])
         else:
             row = [1]
-            for j in range(i-1):
+            for j in xrange(i-1):
                 row.append(triangle[i-1][j] + triangle[i-1][j+1])
             row.append(1)
             triangle.append(row)
